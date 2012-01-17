2a1ad95f62c887b4479212fa4598fbbf3d73703a
Danny Joris <danny_joris@yahoo.co.uk>
2012-01-17 15:22:58 -0400

    adding first draft of solr gmap theming

---
+++ templates/islandora-solr-gmap-items.tpl.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * @file islandora-solr-gmap-items.tpl.php
+ * Islandora solr search marker items template
+ *
+ * Variables available:
+ * - $variables: all array elements of $variables can be used as a variable. e.g. $base_url equals $variables['base_url']
+ * - $base_url: The base url of the current website. eg: http://example.com .
+ * - $user: The user object.
+ *
+ */
+?>
+<?php if($marker_items): ?>
+<ul id="islandora-solr-gmap-items">
+  <?php $zebra = 'odd'; ?>
+  <?php foreach($marker_items as $item): ?>
+     <li class="islandora-solr-gmap-item <?php print $zebra; ?>">
+     <?php $zebra = ($zebra == 'odd'? 'even' : 'odd' ); ?>
+     <div class="img-wrap">
+       <img src="http://164.67.30.146/drupal/fedora/repository/<?php print $item['PID']; ?>/JPG/JPG" />
+     </div>
+       
+     <div class="solr-field-title">
+       <?php $url = 'http://164.67.30.146/drupal/fedora/repository/' . $item['PID']; // @TODO: update this later to base url  ?>
+       <?php $value = (is_array($item['mods_title_s'])? implode(', ', $item['mods_title_s']) : $item['mods_title_s']) ?>
+       <?php print l($value, $url ); ?>
+     </div>  
+   
+   </li>
+  <?php endforeach; ?>
+</ul>
+<?php else: ?>
+  <?php print t('No data found'); ?>
+<?php endif; ?>
